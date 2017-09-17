890272d166a402c8ea8d5d4c8b809bc872c6b607
amartsynkevich <martsynkevich.a@gmail.com>
2017-09-17 17:34:55 +0300

    change in student.java

--- Students/src/Student.java
+++ Students/src/Student.java
@@ -2,8 +2,8 @@ public class Student {
     public String name;
     public String lastName;
 
-    public Student(String name, String lastName) {
-        name = name;
-        lastName = lastName;
+    public Student(String firstname, String surname) {
+        name = firstname;
+        lastName = surname;
     }
    }
